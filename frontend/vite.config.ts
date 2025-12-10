b662482d38173a36cf06c63428bd210d8e622095
Yahman <deman67@protonmail.com>
2025-12-09 21:57:10 -0500

    making sure the vite.config.ts file has the correct port for localhost.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -7,7 +7,10 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000", // or 'http://localhost:3000'
+        target: "http://127.0.0.1:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path,
       },
     },
   },
