a05df1b97b3951a069c76b84091d114d1c18af8b
Yahman <deman67@protonmail.com>
2025-12-10 10:39:58 -0500

    adding host to the vite.config.ts file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '127.0.0.1',        // <--- add this
     port: 5173,
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3000",
+      '/api': {
+        target: 'http://127.0.0.1:3000',  // backend
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
